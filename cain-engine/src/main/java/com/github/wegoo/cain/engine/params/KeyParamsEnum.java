1845d6015290b56f3662f6891d4275a4cb655960
zhenwei1108@163.com <zhenwei1108@163.com>
2024-07-01 22:01:25 +0800

    add key param

---
+++ cain-engine/src/main/java/com/github/wegoo/cain/engine/params/KeyParamsEnum.java
@@ -0,0 +1,22 @@
+package com.github.wegoo.cain.engine.params;
+
+import com.github.wegoo.cain.engine.base.BaseParams;
+import com.github.wegoo.cain.engine.enums.KeyAlgorithmEnum;
+
+public enum KeyParamsEnum implements BaseParams {
+
+  SM2(KeyAlgorithmEnum.SM2, 256),
+  RSA_1024(KeyAlgorithmEnum.RSA, 1024),
+  RSA_2048(KeyAlgorithmEnum.RSA, 2048),
+  RSA_4096(KeyAlgorithmEnum.RSA, 4096),
+  ;
+
+  private KeyAlgorithmEnum keyAlg;
+
+  private int keyLen;
+
+  KeyParamsEnum(KeyAlgorithmEnum keyAlg, int keyLen) {
+    this.keyAlg = keyAlg;
+    this.keyLen = keyLen;
+  }
+}

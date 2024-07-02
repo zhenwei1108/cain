6c6f1823c3f1accb3a87acee91670be03cdc24a4
zhenwei1108@163.com <zhenwei1108@163.com>
2024-07-02 20:10:21 +0800

    add generate key

--- cain-engine/src/main/java/com/github/wegoo/cain/engine/base/BaseProvider.java
+++ cain-engine/src/main/java/com/github/wegoo/cain/engine/base/BaseProvider.java
@@ -1,6 +1,7 @@
 package com.github.wegoo.cain.engine.base;
 
 import com.github.wegoo.cain.engine.params.KeyParamsEnum;
+import java.security.Key;
 import java.security.KeyPair;
 
 public interface BaseProvider {
@@ -9,6 +10,7 @@ public interface BaseProvider {
 
   KeyPair generateKeyPair(KeyParamsEnum keyParams);
 
+  Key generateKey(KeyParamsEnum keyParams);
 
 
 }
